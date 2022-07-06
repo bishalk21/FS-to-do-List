aace579202b112ab4d2ee5ed90f009fe8ac9bacb
Your Name <you@example.com>
2022-07-06 17:22:08 +1000

    remove gitignore files

--- src/Components/TaskForm.js
+++ src/Components/TaskForm.js
@@ -3,7 +3,7 @@ import { Form } from "react-bootstrap";
 import Row from "react-bootstrap/Row";
 import Button from "react-bootstrap/Button";
 import { useState } from "react";
-import { v4 as uuidv4 } from "uuid";
+import { v4 as uuidv4 } from "uuid"; //uuid
 
 const initialState = {
   task: "",
@@ -37,6 +37,7 @@ export const TaskForm = ({ addTask }) => {
             placeholder="Task Name"
             required
             onChange={handleOnChange}
+            l
           />
         </Col>
         <Col md="3">
@@ -58,3 +59,12 @@ export const TaskForm = ({ addTask }) => {
     </Form>
   );
 };
+
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// Example:
+// Given nums = [2, 7, 11, 15], target = 9,
+
+//nums is an array of integers, target is an integer
+// twoSum is a function that takes in an array of integers and an integer
+var twoSum = function (nums, target) {};
